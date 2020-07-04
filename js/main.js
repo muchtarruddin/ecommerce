22b60349d0ecdf2146149ee3646144629c8a8866
muchtarruddin <muchtarruddin99@gmail.com>
2020-07-05 01:52:54 +0700

    ui update

--- js/main.js
+++ js/main.js
@@ -9,3 +9,15 @@ $(".slider-one")
         prevArrow: ".site-slider .slider-btn .prev",
         nextArrow: ".site-slider .slider-btn .next"
     });
+
+/**Second Slider */
+
+$(".slider-two")
+    .not(".slick-intialized")
+    .slick({
+        prevArrow: ".site-slider-two .prev",
+        nextArrow: ".site-slider-two .next",
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplaySpeed: 3000
+    });
