9b9177c8460f775701f22965b9a6c147dcd3cb92
yehe12 <yehe12qw12@gmail.com>
2020-07-08 20:10:29 +0700

    tombol

--- ci-app/application/views/Tabel_Admin/index.php
+++ ci-app/application/views/Tabel_Admin/index.php
@@ -11,7 +11,10 @@
             <h3> Daftar Admin </h3>
             <ul class="list-group">
             <?php foreach( $tabel_admin as $ta ) : ?>
-                <li class="list-group-item"><?= $ta['nama_admin']; ?></li>
+                <li class="list-group-item">
+                <?= $ta['nama_admin']; ?>
+                <<a href="<?= base_url(); ?>Tabel_Admin/hapus">hapus</a>
+                </li>
             <?php endforeach; ?>
             </ul>
         </div>
